385473531deafa47649c389ed98dca536f65a75e
Hazem Gado <hazem.gado@gmail.com>
2023-05-03 23:56:50 +0300

    plz test work

--- test.py
+++ test.py
@@ -1,37 +1,73 @@
-import argparse
 import os
+import argparse
+from tqdm import tqdm
 
-import cv2
-import numpy as np
-import tensorflow as tf
+import torch
+import torchvision.transforms as transforms
+from PIL import Image
 
-from models.networks import FPN
-from utils.config import process_config
-from utils.utils import get_args
-from utils.image_utils import load_image, save_image
+def get_generator(model_type):
+    if model_type == 'resnet':
+        generator = ...
+    elif model_type == 'unet':
+        generator = ...
+    else:
+        raise ValueError('Invalid model_type: {}'.format(model_type))
+    return generator
 
-def test_model(config, model_path, test_data_path, result_path):
-    # Load model
-    model = FPN(config)
+def tensor2im(tensor):
+    tensor = tensor.squeeze(0).detach().cpu()
+    tensor = tensor.mul(0.5).add(0.5)
+    tensor = tensor.clamp(0, 1)
+    tensor = tensor.permute(1, 2, 0).numpy()
+    tensor = (tensor * 255).astype('uint8')
+    return tensor
 
-    # Load saved weights
-    model.load_weights(model_path)
+def mkdirs(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
 
-    # Load test image
-    test_image = load_image(test_data_path)
+def main(args):
+    # Create output directory
+    mkdirs(args.output_dir)
 
-    # Predict deblurred image
-    predicted_image = model.predict(np.expand_dims(test_image, axis=0))[0]
+    # Load generator
+    generator = get_generator('resnet')
+    generator.load_state_dict(torch.load(args.model_path)['model'])
+    generator.eval()
 
-    # Save result
-    save_image(predicted_image, result_path)
+    # Load transforms
+    transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.5, 0.5, 0.5],
+                             std=[0.5, 0.5, 0.5])
+    ])
+
+    # Get files
+    files = get_files(args.input_dir, args.output_dir)
+
+    # Generate images
+    for blur_path, sharp_path in tqdm(files):
+        blur_img = Image.open(blur_path).convert('RGB')
+        blur_tensor = transform(blur_img).unsqueeze(0)
+
+        with torch.no_grad():
+            sharp_tensor = generator(blur_tensor)
+
+        sharp_img = tensor2im(sharp_tensor)
+        sharp_pil = Image.fromarray(sharp_img)
+
+        # Save result
+        sharp_pil.save(sharp_path)
 
 if __name__ == '__main__':
-    # Parse command-line arguments
-    args = get_args()
+    parser = argparse.ArgumentParser(description='Deblur images')
+    parser.add_argument('--input_dir', type=str, required=True,
+                        help='Path to directory containing blurred images')
+    parser.add_argument('--output_dir', type=str, required=True,
+                        help='Path to directory to save deblurred images')
+    parser.add_argument('--model_path', type=str, required=True,
+                        help='Path to generator model checkpoint')
+    args = parser.parse_args()
 
-    # Load configuration file
-    config = process_config(args.config)
-
-    # Test the model
-    test_model(config, args.model_path, args.test_data_path, args.result_path)
+    main(args)
